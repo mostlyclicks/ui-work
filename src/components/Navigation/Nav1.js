b4f844d4361c4d74e74bc438bafc32a8a6dbcd16
mostlyclicks <carlos@mostlyclicks.com>
2021-08-10 14:07:22 -0600

    add styled-components, style mobile/desktop nav

--- src/components/Navigation/Nav1.js
+++ src/components/Navigation/Nav1.js
@@ -1,23 +1,58 @@
 import React, { Fragment } from 'react'
+import styled from 'styled-components'
 
 const Nav1 = (props) => {
 
   const navLinks = props.navLinks
-  console.log(navLinks)
-
-
+ 
   return (
     <Fragment>
-      <h2>Navigation 1</h2>
-      <ul>
+      <StyledUl>
         {navLinks.map((navLink, index) => (
           <li key={index}><a href={navLink.urlPath}>{navLink.linkName}</a></li>
         ))}
-
-      </ul>
-      
+      </StyledUl>
     </Fragment>
   )
 }
 
 export default Nav1
+
+
+const StyledUl =  styled.ul`
+ display:flex;
+ justify-content:flex-end;
+ align-items:center;
+ flex-direction:column;
+ width:100%;
+ margin:0;
+ padding:0;
+ list-style:none;
+ li {
+   display:flex;
+   width:90%;
+   border-bottom:1px solid #cdcdcd;
+   &:last-child{border-bottom:1px solid transparent;}
+   a {
+     display:block;
+     width:100%;
+     padding:1rem;
+     text-align:center;
+   }
+ }
+ 
+ 
+
+ @media (min-width:768px) {
+   flex-direction:row;
+   li {
+     border:none;
+     width:auto;
+     a {
+       padding:1rem 2rem;
+     }
+   }
+   
+ }
+
+`
