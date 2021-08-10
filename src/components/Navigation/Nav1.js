8b5ea6f0215b7df7fb00c6ac0bc38166cddb8ec8
mostlyclicks <carlos@mostlyclicks.com>
2021-08-10 10:47:17 -0600

    add nav1

---
+++ src/components/Navigation/Nav1.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react'
+
+const Nav1 = () => {
+  return (
+    <Fragment>
+      <h2>Navigation 1</h2>
+      <ul>
+        <li>Nav item</li>
+        <li>Nav item</li>
+        <li>Nav item</li>
+        <li>Nav item</li>
+        <li>Nav item</li>
+      </ul>
+      
+    </Fragment>
+  )
+}
+
+export default Nav1
